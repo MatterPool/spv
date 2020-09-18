309eb88b454748a6c0f936451d2f2666c475e44b
Dean <dean@bitping.com>
2020-09-18 17:34:45 +1000

    Added documentation and tests

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const spv = module.exports
-spv.SPV = require('./lib/spv')
-spv.SPVTx = require('./lib/spv-tx')
+import { SPV } from './lib/spv.js'
+import { SPVTx } from './lib/spv-tx.js'
+
+export {
+    SPV,
+    SPVTx
+}
